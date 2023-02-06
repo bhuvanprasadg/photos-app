0a4d39210ad1b9ccebc43b4cc9672b47fa059eb5
Bhuvan Prasad G <101169759+bhuvi-12@users.noreply.github.com>
2023-02-06 13:05:57 +0530

    Update Jenkinsfile_declarative.groovy

--- Jenkinsfile_declarative.groovy
+++ Jenkinsfile_declarative.groovy
@@ -16,38 +16,43 @@ pipeline{
   }
   stages{
     stage('Code checkout'){
-      script{
+      steps{
         git branch: "${GITHUB_BRANCH}", url: "${GITHUB_PROJECT_URL}", credentialsId: "${GITHUB_CREDENTIALS}"
       }
     }
     
     stage('Docker Image Build & Push to ECR'){
-      script{
-        BUILD_NUMBER = currentBuild.number
-        IMAGE_VERSION = "v${BUILD_NUMBER}"
-        docker.withRegistry(
-            "https://${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com",
-            "ecr:${AWS_REGION}:${AWS_JENKINS_CREDENTIALS_ID}") {
-            def dockerImage = docker.build("${AWS_ECR_IMAGE}:${IMAGE_VERSION}")
-            dockerImage.push()
+      steps{
+        script{
+          BUILD_NUMBER = currentBuild.number
+          IMAGE_VERSION = "v${BUILD_NUMBER}"
+          docker.withRegistry(
+              "https://${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com",
+              "ecr:${AWS_REGION}:${AWS_JENKINS_CREDENTIALS_ID}") {
+              def dockerImage = docker.build("${AWS_ECR_IMAGE}:${IMAGE_VERSION}")
+              dockerImage.push()
+        }
       }
     }
     
     stage("Docker image scan"){
+      steps{
         sh "trivy --no-progress --severity HIGH,CRITICAL ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_IMAGE}:${IMAGE_VERSION}"
+      }
     }
     
     stage('Deploying ECR image to EKS cluster'){
-      script{
-        BUILD_NUMBER = currentBuild.number
-        IMAGE_VERSION = "v${BUILD_NUMBER}"
-        withAWS(credentials: "${AWS_JENKINS_CREDENTIALS_ID}", region: "${AWS_REGION}") {
-              sh "aws eks update-kubeconfig --region ${AWS_REGION} --name ${AWS_EKS_CLUSTER_NAME}"
-              sh "kubectl set image deployment.apps/${EKS_DEPLOYMENT_NAME} -n ${EKS_NAMESPACE} ${RUNNING_CONTAINER_NAME}=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_IMAGE}:${IMAGE_VERSION}"
-              sh "kubectl rollout status deployment/${EKS_DEPLOYMENT_NAME} -n ${EKS_NAMESPACE}"
+      steps{
+          script{
+          BUILD_NUMBER = currentBuild.number
+          IMAGE_VERSION = "v${BUILD_NUMBER}"
+          withAWS(credentials: "${AWS_JENKINS_CREDENTIALS_ID}", region: "${AWS_REGION}") {
+                sh "aws eks update-kubeconfig --region ${AWS_REGION} --name ${AWS_EKS_CLUSTER_NAME}"
+                sh "kubectl set image deployment.apps/${EKS_DEPLOYMENT_NAME} -n ${EKS_NAMESPACE} ${RUNNING_CONTAINER_NAME}=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_IMAGE}:${IMAGE_VERSION}"
+                sh "kubectl rollout status deployment/${EKS_DEPLOYMENT_NAME} -n ${EKS_NAMESPACE}"
+          }
         }
       }
     }
   }
-}
 }
