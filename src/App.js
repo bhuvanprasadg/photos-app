d04fd2bf0bb1929eddc95ef39bcfeaad6c07335c
Bhuvan Prasad G <101169759+bhuvanprasadg@users.noreply.github.com>
2023-03-27 15:33:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className='App'>
             <h1>
-                <b>Welcome to Gallery Application by Bhuvan</b>
+                <b>Welcome to Gallery Application by Bhuvan - test</b>
             </h1>
             <Photos />
         </div>
