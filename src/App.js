35740e18d05f0d43ea7e207c2679635815a543bb
Bhuvan Prasad G <101169759+bhuvanprasadg@users.noreply.github.com>
2023-03-28 16:28:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className='App'>
             <h1>
-                <b>Welcome to Gallery Application</b>
+                <b>Welcome to Sample Gallery Application for testing</b>
             </h1>
             <Photos />
         </div>
